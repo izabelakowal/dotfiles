c4c5f892747e6f1e9db2605be3addce898d3aea8
Izabela Kowal <izabelakowal@protonmail.com>
2021-02-05 13:50:05 +0100

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,14 @@
+# shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias unpack='tar xvzf'
+alias pack='tar zcvf
+
+# directories
+alias dotfiles="cd $DOTFILES"
+
+# git
+alias gl="git log --oneline --decorate --color"
+
+# docker
+alias drmiall='docker rmi $(sudo docker images)'
+alias drmi_prune='docker system prune'
