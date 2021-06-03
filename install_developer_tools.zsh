bba9a2d8ed1dda6d113c1736ed59f1c9b024d69b
izabelakowal <hello@izabelakowal.com>
2021-06-04 00:53:44 +0200

    Remove python related installations from general developer tools installer

--- install_developer_tools.zsh
+++ install_developer_tools.zsh
@@ -1,19 +1,4 @@
 #!/usr/bin/env zsh
 
-echo "Installing VSCode extensions...\n"
+echo "\nInstalling VSCode extensions...\n"
 cat vscode_extensions | xargs -L 1 code --install-extension
-
-# install pyenv
-# TODO check with exists
-echo "Setting up pyenv...\n"
-brew install openssl readline sqlite3 xz zlib
-curl https://pyenv.run | bash
-# TODO check:
-# which pyenv
-# echo $PATH | grep --color=auto "$(pyenv root)/shims"
-# TODO install recent version
-
-# setup pipx
-# TODO check with exists
-brew install pipx
-pipx ensurepath
