25365f48e28322f4f24ffcff24d41914ab334598
izabelakowal <hello@izabelakowal.com>
2021-06-05 18:05:38 +0200

    Fix path to extensions file

--- vscode/install_vscode_tools.zsh
+++ vscode/install_vscode_tools.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 echo "\nInstalling VSCode extensions...\n"
-cat vscode_extensions | xargs -L 1 code --install-extension
+cat $HOME/.dotfiles/vscode/vscode_extensions | xargs -L 1 code --install-extension
